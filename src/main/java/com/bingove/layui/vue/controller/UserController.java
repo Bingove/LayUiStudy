c1e94ef9c044eac7f99ae43dcebb8802dd578c14
Bingove <15138596568@163.com>
2019-05-17 08:53:56 +0800

    user

---
+++ src/main/java/com/bingove/layui/vue/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.bingove.layui.vue.controller;
+
+/**
+ * @projectName LayUiStudy
+ * @Author 常冬军
+ * @Date 2019/5/17 0017上午 8:53
+ * @title: UserController
+ * @ToDo
+ */
+public class UserController {
+}
